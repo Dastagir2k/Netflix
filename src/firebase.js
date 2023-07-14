7ac16fb3efae623ddb94608bff0f91e4ba92ce6a
Dastagir <dastagir2k@gmail.com>
2023-07-14 12:42:12 +0530

    Login Screen Finished

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBscRIJHXz8J1fyKRINtYxnXX41hhdhLVg",
   authDomain: "netflix-7ae6f.firebaseapp.com",
   projectId: "netflix-7ae6f",
   storageBucket: "netflix-7ae6f.appspot.com",
   messagingSenderId: "343999036540",
-  appId: "1:343999036540:web:13c4be8e4050ec7f0c1df9"
+  appId: "1:343999036540:web:13c4be8e4050ec7f0c1df9",
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getAuth(app);
+export default db;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,17 +1,40 @@
 import React from "react";
 import "./SignUpScreen.css";
+import { useRef } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import db, { auth } from "../firebase";
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+  const register = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passRef.current.value;
+    createUserWithEmailAndPassword(db, email, password)
+      .then((authuser) => {
+        console.log("Data", authuser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="SignUpScreen">
       <form>
         <h1>Sign in</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="password" type="passsword" />
-        <button>Sign In </button>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passRef} placeholder="password" type="passsword" />
+        <button onClick={signIn}>Sign In </button>
         <h4>
           <span className="SignUpScreen_gray">New to Netflix ? </span>
-          <span className="SignUpScreen_link">SignUp Now . </span>
+          <span className="SignUpScreen_link" onClick={register}>
+            SignUp Now .{" "}
+          </span>
         </h4>
       </form>
     </div>
